06c2a8a6634cd98ea17c9c872aa391500d6ef065
Dipankar Yadav <152011331+dipankar9794@users.noreply.github.com>
2026-03-06 00:17:55 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (76.58%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -4,7 +4,7 @@ public:
         int l = 0;
         int r = nums.size() - 1;
         while (l <= r){
-            int m = l + (r-l)/2;
+            int m = (l+r) /2;
             if (nums[m]==target){
                 return m;
               }
